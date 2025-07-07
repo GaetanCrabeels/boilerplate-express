b6354c7e6032f67cece3634ed8d0137f012782fb
gaetancrabeels@hotmail.com <Crabeels.gaetan@outlook.be>
2025-07-07 13:59:39 +0200

    Création d'API et utilisation de Node.js/NPM

--- myApp.js
+++ myApp.js
@@ -1,39 +1,45 @@
+require('dotenv').config();
+const bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
 
 
+app.use(bodyParser.urlencoded({ extended: false }));        
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
 
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/views/index.html');
+});
 
+app.get('/json', (req, res) => {
+  let message = 'Hello json';
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    message = message.toUpperCase();
+    console.log(message)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+  }
+  res.json({ message });
+});
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({ time: req.time });
+});
+app.get('/:word/echo', (req, res) => {
+  res.json({ echo: req.params.word });
+});
+app.get('/name', (req, res) => {
+  const { first: firstName, last: lastName } = req.query;
+  res.json({ name: `${firstName} ${lastName}` });
+});
+app.post('/name', (req, res) => {
+  const { first: firstName, last: lastName } = req.body;
+  res.json({ name: `${firstName} ${lastName}` });
+});
+module.exports = app;
