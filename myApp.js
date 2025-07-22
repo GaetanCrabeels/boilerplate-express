b2cac9684700c607aaf1fdd48944a7ee48673368
gaetancrabeels@hotmail.com <Crabeels.gaetan@outlook.be>
2025-07-22 09:28:48 +0200

    a

--- myApp.js
+++ myApp.js
@@ -6,10 +6,6 @@ let app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));        
 
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.path} - ${req.ip}`);
-    next();
-});
 
 app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
@@ -18,7 +14,7 @@ res.sendFile(__dirname + '/views/index.html');
 
 app.get('/json', (req, res) => {
   let message = 'Hello json';
-  if (process.env.MESSAGE_STYLE === 'uppercase') {
+  if (process.env.MESSAGE_STYLE) {
     message = message.toUpperCase();
     console.log(message)
 
